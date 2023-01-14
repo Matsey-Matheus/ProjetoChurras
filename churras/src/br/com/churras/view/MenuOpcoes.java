34a50b2c04004a8e42060040cd706ef19d62b617
gerandofalcoes <gerandofalcoes@192.168.1.106>
2023-01-14 12:03:55 -0300

    add classe Main e MenuOpcoes

---
+++ churras/src/br/com/churras/view/MenuOpcoes.java
@@ -0,0 +1,10 @@
+package br.com.churras.view;
+
+public class MenuOpcoes {
+	public void menuOpcoes() {
+		System.out.println("informacoes sobre as comidas");
+		System.out.println("1 - cadastro de convidados \n2 - Cadastro de comidas \n3 - Cadastro de bebidas \n4exibir valor por pessoa");
+
+	}
+
+}
--- churras/src/br/com/churras/view/View.java
+++ churras/src/br/com/churras/view/View.java
@@ -3,7 +3,34 @@ package br.com.churras.view;
 public class View {
 
 	public static void main(String[] args) {
-		
+		MenuOpcoes menuOpcoes = new MenuOpcoes();
+ // teste
+		int escolhaUsuario = 0;
+		do {
+			switch (escolhaUsuario) {
+			case 1: { // cadastrar convidados
+				
+				break;
+			}
+			case 2: { // cadastrar carnes
+
+				break;
+			}
+			case 3: { // cadastrar bebidas
+
+				break;
+			}
+			case 4: { // exibir valor por pessoa
+
+				break;
+			}
+			default:
+				System.out.println("default");
+			}
+			menuOpcoes.menuOpcoes();
+
+		} while (true);
+
 	}
 	
 }
