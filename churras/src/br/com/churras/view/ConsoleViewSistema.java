02e2a357c6891b38a05f2b51adf9171e0110a5c8
Bruno Polido <brunopolidoandrade@gmail.com>
2023-01-22 20:49:19 -0300

    asd

--- churras/src/br/com/churras/view/ConsoleViewSistema.java
+++ churras/src/br/com/churras/view/ConsoleViewSistema.java
@@ -203,7 +203,7 @@ public class ConsoleViewSistema {
 
 	/**
 	 * TODO CRIAR MENSAGEM DE INTRODUÇÃO (COMO USAR NOSSO PROGRAMA)
-	 *  
+	 *  sadasd
 	 * @author Bruno Polido
 	 */
 	public void introducao() {
