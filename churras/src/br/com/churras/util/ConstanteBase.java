e71691f91da2a67efff4e49db3e99cadb472a502
Miguel Arcanjo <945507756m@gmail.com>
2023-02-04 10:46:27 -0300

    add Constantes no BaseView | add mensagens no omponent

---
+++ churras/src/br/com/churras/util/ConstanteBase.java
@@ -0,0 +1,8 @@
+package br.com.churras.util;
+
+public class ConstanteBase {
+
+	
+	
+	
+}
--- churras/src/br/com/churras/view/MensagemBaseView.java
+++ churras/src/br/com/churras/view/MensagemBaseView.java
@@ -5,9 +5,9 @@ public abstract class MensagemBaseView {
 	public void printaMensagem(String mensagem) {
 		System.out.println(mensagem);
 	}
-	
+
 	public void printaMensagemErro(String mensagem) {
 		System.err.println(mensagem);
 	}
-	
+
 }
